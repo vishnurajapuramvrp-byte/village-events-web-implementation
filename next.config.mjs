41befe740546520c274536fba43df99340d4b8b1
Vishnu Raja Puram <vishnurajapuram.vrp@gmail.com>
2026-09-24 16:46:19 +0530

    Fix Vercel PDF generation

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   experimental: {
     serverComponentsExternalPackages: ["@react-pdf/renderer", "@prisma/client", "bcryptjs"],
+    outputFileTracingIncludes: {
+      "/*": [
+        "./node_modules/pdfkit/**/*",
+        "./node_modules/@react-pdf/renderer/node_modules/pdfkit/**/*",
+      ],
+      "/api/reports/[id]/pdf": [
+        "./node_modules/pdfkit/js/standard-fonts/**/*",
+        "./node_modules/pdfkit/js/data/**/*",
+        "./node_modules/@react-pdf/renderer/node_modules/pdfkit/js/standard-fonts/**/*",
+        "./node_modules/@react-pdf/renderer/node_modules/pdfkit/js/data/**/*",
+      ],
+    },
   },
 };
 
